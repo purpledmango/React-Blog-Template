6a6cd870f126f6d6d8062f04ab6aa57293c565df
purpledmango <nitishmerlin@gmail.com>
2024-04-21 12:18:10 +0530

    Article Page UI fix

--- src/pages/ArticlePage.jsx
+++ src/pages/ArticlePage.jsx
@@ -4,28 +4,41 @@ import { getArticleApi } from "../services/homeApi";
 import Spinner from "../components/Spinner";
 import { formatDate } from "../utlis/dateFormatter";
 import { HOST_NAME } from "../constants/hosts";
+import { formatAuthorName } from "../utlis/authorNames";
 
 const Article = () => {
   const { slug } = useParams();
   const [article, setArticle] = useState(null);
+  const [authorName, setAuthorName] = useState("Unknown");
+
+  const fetchArticleData = async () => {
+    try {
+      const fetchedArticle = await getArticleApi(slug);
+      console.log("Fetched Article", fetchedArticle);
+      setArticle(fetchedArticle.data);
+    } catch (error) {
+      console.error("Error fetching article:", error);
+    }
+  };
 
   useEffect(() => {
-    const fetchArticle = async () => {
-      try {
-        const fetchedArticle = await getArticleApi(slug);
-        console.log("Fetched Article", fetchedArticle);
-        setArticle(fetchedArticle.data);
-      } catch (error) {
-        console.error("Error fetching article:", error);
-      }
-    };
-
-    fetchArticle();
+    fetchArticleData();
   }, [slug]);
 
   useEffect(() => {
     if (article) {
       document.title = `${article.title} • PatchedWeb`;
+
+      const fetchAuthorName = async () => {
+        try {
+          const name = await formatAuthorName(article.author);
+          setAuthorName(name);
+        } catch (error) {
+          console.error("Error fetching author name:", error);
+        }
+      };
+
+      fetchAuthorName();
     }
   }, [article]);
 
@@ -38,27 +51,23 @@ const Article = () => {
   }
 
   return (
-
-
     <div className="my-5 w-full">
       <div className="w-full flex justify-evenly items-center tracking-wide lg:p-4 lg:m-5 sm:flex-none">
         <span className="uppercase text-xs">{formatDate(article.createdAt) || "N/A"}</span>
         <span className="uppercase text-xs">{article.category || "N/A"}</span>
         <span className="uppercase text-xs">
-          BY,<span className="text-orange-400">{article.author || "Unknown"}</span>
+          BY,<span className="text-orange-400">{authorName}</span>
         </span>
       </div>
       <div className="grid lg:grid-cols-4">
         {/* for banners ord ads */}
-        <div className="lg:col-span-1">
-
-        </div>
+        <div className="lg:col-span-1"></div>
         <div className="lg:col-span-2 p-5 lg:my-5">
           <h1 className="text-4xl py-5 lg:my-5">{article.title}</h1>
           {/* Render article content with HTML tags safely */}
           <img
             src={`${HOST_NAME}/${article.thumbnail.path}`}
-            className="w-full h-[300px] lg:h-[480px] xl:h-[650px]  object-contain rounded-xl"
+            className="w-full h-[300px] lg:h-[480px] xl:h-[650px] object-contain rounded-xl"
             alt={article.thumbnail.filename}
           />
           <p id="content" dangerouslySetInnerHTML={{ __html: article.content || "" }} />
@@ -66,7 +75,6 @@ const Article = () => {
         <div className="lg:col-span-1"></div>
       </div>
     </div>
-
   );
 };
 
