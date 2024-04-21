0a1947b0b7ec06ba5cf55d1364e6ad2caea7e90b
purpledmango <nitishmerlin@gmail.com>
2024-04-21 12:31:27 +0530

    integrated Analytics

--- src/views/Base.jsx
+++ src/views/Base.jsx
@@ -1,11 +1,19 @@
+import { useEffect } from "react";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Nav from "../components/Nav";
 import Home from "../pages/Home";
 import ArticlePage from "../pages/ArticlePage";
 import About from "../pages/About";
 import Footer from "../components/Footer";
+import ReactGA from 'react-ga';
+const TRACKING_ID = "G-JPJ3VWY20K";
+
+ReactGA.initialize(TRACKING_ID);
 
 const Base = () => {
+    useEffect(() => {
+        ReactGA.pageview(window.location.pathname + window.location.search);
+    }, []);
     return (
         <BrowserRouter>
             <Nav />
